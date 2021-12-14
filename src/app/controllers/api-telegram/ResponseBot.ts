3f6653d9eeee25d5277d2baf214a3359cf68692f
Manuel Florez <manuelflorezw@gmail.com>
2021-12-13 21:18:38 -0500

    configiurando logs a trazas

--- src/app/controllers/api-telegram/ResponseBot.ts
+++ src/app/controllers/api-telegram/ResponseBot.ts
@@ -5,21 +5,21 @@ import { GetUpdates } from '../../dtos/getUpdates'
 export class ResponseBot {
 
   getMet(me: Me) {
-    console.log(`ok: `, me.ok)
-    console.log(`result: `, me.result)
+    this.log(`ok: `, me.ok)
+    this.log(`result: `, me.result)
   }
 
   logOut(logOut: LogOut) {
-    console.log(`description: `, logOut.description)
-    console.log(`error_code: `, logOut.error_code)
-    console.log(`result: `, logOut.result)
-    console.log(`ok: `, logOut.ok)
+    this.log(`description: `, logOut.description)
+    this.log(`error_code: `, logOut.error_code)
+    this.log(`result: `, logOut.result)
+    this.log(`ok: `, logOut.ok)
   }
 
   getUpdates(getUpdates: GetUpdates) {
     getUpdates.result.forEach((data) => {
       const msg = `${data.update_id}`
-      console.log(msg)
+      this.log(msg)
     })
   }
 
@@ -27,6 +27,12 @@ export class ResponseBot {
     console.log(getUpdates.ok)
   }
 
-  
+  log(arg1: string, arg2?: any) {
+    if (arg2 === undefined) {
+      console.log(arg1)
+    } else {
+      console.log(arg1, arg2)
+    }
+  }
 
 }
